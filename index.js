3387d6a7e711915f46e094f6e6ebf9d6594841ad
Nabya Naseem <146648831+nabya47@users.noreply.github.com>
2024-11-14 15:17:35 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
+
+const authRoutes = require("./routes/auth.js");
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+/* ROUTES */
+app.use("/auth", authRoutes);
+
+
+/* MONGOOSE SETUP */
+const PORT = 3000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    dbName: "Royal_Blue",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+  })
+  .catch((err) => console.log(`${err} did not connect`));
